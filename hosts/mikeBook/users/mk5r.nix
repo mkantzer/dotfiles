84578bcd6e13e429f5d4a3ebe1793084c2181a5e
Mike Kantzer <mbkantzer@gmail.com>
2022-09-17 12:43:02 -0400

    update

--- hosts/mikeBook/users/mk5r.nix
+++ hosts/mikeBook/users/mk5r.nix
@@ -13,14 +13,13 @@
       # ../../../modules/graphical-home.nix
       # ../../../modules/thinkpad-home.nix
     ];
+    # openssh.authorizedKeys.keys = [
+    #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9K7mYtXECI6LD8iptulJC3eXZL4fE+M1M24UftlcnX"
+    #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFnW0M2ZOK4/q5zQgMb03Cvq+DkVGaOEcqbozJXbVWh"
+    # ];
   };
 }
 
 
 
 
-
-    # openssh.authorizedKeys.keys = [
-    #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9K7mYtXECI6LD8iptulJC3eXZL4fE+M1M24UftlcnX"
-    #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFnW0M2ZOK4/q5zQgMb03Cvq+DkVGaOEcqbozJXbVWh"
-    # ];
