f8406a6928af7e24609a8ea6058e3cc8404beb40
Mike Kantzer <mbkantzer@gmail.com>
2022-09-26 08:07:36 -0400

    remove some zsh config that might be breaking stuff

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -6,19 +6,19 @@
     enableSyntaxHighlighting = true;
     defaultKeymap = "viins";
 
-    initExtra = ''
-      path=("$HOME/.cargo/bin" $path)
-      path+=("$HOME/.local/bin")
+    # initExtra = ''
+    #   path=("$HOME/.cargo/bin" $path)
+    #   path+=("$HOME/.local/bin")
 
-      lt() {
-          exa --icons --color=always --git-ignore -TL $1 $2 | bat -p
-      }
-      lta() {
-          exa --icons --color=always -aT $1 | bat -p
-      }
+    #   lt() {
+    #       exa --icons --color=always --git-ignore -TL $1 $2 | bat -p
+    #   }
+    #   lta() {
+    #       exa --icons --color=always -aT $1 | bat -p
+    #   }
 
-      PROMPT=$'\n'"%F{6}%~%f"$'\n'"%B%(#.%F{1}.%F{2})%# %f%b"
-    '';
+    #   PROMPT=$'\n'"%F{6}%~%f"$'\n'"%B%(#.%F{1}.%F{2})%# %f%b"
+    # '';
   };
 
   home.sessionVariables = {
