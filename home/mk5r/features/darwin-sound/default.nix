0e94bfd7831799a3f03016d0f9138d880a393e24
Mike Kantzer <mbkantzer@gmail.com>
2022-11-17 10:48:04 -0500

    fixup. its gotta be executable!

--- home/mk5r/features/darwin-sound/default.nix
+++ home/mk5r/features/darwin-sound/default.nix
@@ -1,38 +1,44 @@
 { config, lib, ... }:
 
 {
-  home.file."Library/Application Scripts/net.mousedown.EventScripts/switchAudioPlugIn.sh".text = ''
-#!/bin/bash
+  home.file."Library/Application Scripts/net.mousedown.EventScripts/switchAudioPlugIn.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/bash
 
-sleep 1
+      sleep 1
 
-docked_output_count=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -r '.name' | grep 'CalDigit Thunderbolt 3 Audio\|Yeti Stereo Microphone' | wc -l | xargs)
+      docked_output_count=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -r '.name' | grep 'CalDigit Thunderbolt 3 Audio\|Yeti Stereo Microphone' | wc -l | xargs)
 
-if [[ ''${docked_output_count} -gt 0 ]]
-then
-  LOOPBACK_OUTPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="LoopBack") | .id')
-  /opt/homebrew/bin/SwitchAudioSource -t output -i $LOOPBACK_OUTPUT_ID
-  YETI_OUTPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t input | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="Yeti Stereo Microphone") | .id')
-  /opt/homebrew/bin/SwitchAudioSource -t input -i $YETI_OUTPUT_ID
-else
-  echo "not switching"
-fi
-  '';
-  home.file."Library/Application Scripts/net.mousedown.EventScripts/switchAudioPlugOut.sh".text = ''
-#!/bin/bash
+      if [[ ''${docked_output_count} -gt 0 ]]
+      then
+        LOOPBACK_OUTPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="LoopBack") | .id')
+        /opt/homebrew/bin/SwitchAudioSource -t output -i $LOOPBACK_OUTPUT_ID
+        YETI_OUTPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t input | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="Yeti Stereo Microphone") | .id')
+        /opt/homebrew/bin/SwitchAudioSource -t input -i $YETI_OUTPUT_ID
+      else
+        echo "not switching"
+      fi
+    '';
+  };
+  home.file."Library/Application Scripts/net.mousedown.EventScripts/switchAudioPlugOut.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/bash
 
-sleep 1
+      sleep 1
 
-docked_output_count=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -r '.name' | grep 'CalDigit Thunderbolt 3 Audio\|Yeti Stereo Microphone' | wc -l | xargs)
+      docked_output_count=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -r '.name' | grep 'CalDigit Thunderbolt 3 Audio\|Yeti Stereo Microphone' | wc -l | xargs)
 
-if [[ ''${docked_output_count} -eq 0 ]]
-then
-  LAPTOP_OUTPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="MacBook Pro Speakers") | .id')
-  /opt/homebrew/bin/SwitchAudioSource -t output -i $LAPTOP_OUTPUT_ID
-  LAPTOP_INPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t input | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="MacBook Pro Microphone") | .id')
-  /opt/homebrew/bin/SwitchAudioSource -t input -i $LAPTOP_INPUT_ID
-else
-  echo "not switching"
-fi
-  '';
+      if [[ ''${docked_output_count} -eq 0 ]]
+      then
+        LAPTOP_OUTPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t output | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="MacBook Pro Speakers") | .id')
+        /opt/homebrew/bin/SwitchAudioSource -t output -i $LAPTOP_OUTPUT_ID
+        LAPTOP_INPUT_ID=$(/opt/homebrew/bin/SwitchAudioSource -a -f json -t input | /Users/${config.home.username}/.nix-profile/bin/jq -sr '.[] | select(.name=="MacBook Pro Microphone") | .id')
+        /opt/homebrew/bin/SwitchAudioSource -t input -i $LAPTOP_INPUT_ID
+      else
+        echo "not switching"
+      fi
+    '';
+  };
 }
