424cbb4998e34dd19dc69592085859744608f9e2
mk5r <github@kantzer.io>
2022-11-24 09:33:56 -0500

    unify packages file with a distroSpecific

--- home/mk5r/features/nvim/default.nix
+++ home/mk5r/features/nvim/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
-{ 
+{
   home.sessionVariables.EDITOR = "nvim";
 
   programs.neovim = {
@@ -10,49 +10,66 @@
     vimAlias = true;
     vimdiffAlias = true;
 
-  #  extraConfig = ''
-  #     syntax on 
-  #     " --- visual ---
-  #     colorscheme onedark
-  #     let g:lightline = {
-  #         \ 'colorscheme': 'onedark',
-  #         \ }
-  #     set background=dark
-      
-  #     set termguicolors
-  #     set number
-  #     set noruler
-  #     set wrap
-  #     set showmatch
-  #     set matchtime=3
-  #     set list
-  #     highlight Comment cterm=italic gui=italic
-  #     set nofoldenable
-  #     let g:vim_markdown_frontmatter = 1
-  #     let g:vim_markdown_strikethrough = 1
-  #     " --- grep ---
-  #     set ignorecase
-  #     set smartcase
-  #     set wrapscan
-  #     set hlsearch
-  #     set incsearch
-  #     " --- indent ---
-  #     set smartindent
-  #     set expandtab
-  #     " --- keymap ---
-  #     " jk as esc
-  #     inoremap <silent> jk <Esc>
-  #   '';
+    #  extraConfig = ''
+    #     syntax on 
+    #     " --- visual ---
+    #     colorscheme onedark
+    #     let g:lightline = {
+    #         \ 'colorscheme': 'onedark',
+    #         \ }
+    #     set background=dark
+
+    #     set termguicolors
+    #     set number
+    #     set noruler
+    #     set wrap
+    #     set showmatch
+    #     set matchtime=3
+    #     set list
+    #     highlight Comment cterm=italic gui=italic
+    #     set nofoldenable
+    #     let g:vim_markdown_frontmatter = 1
+    #     let g:vim_markdown_strikethrough = 1
+    #     " --- grep ---
+    #     set ignorecase
+    #     set smartcase
+    #     set wrapscan
+    #     set hlsearch
+    #     set incsearch
+    #     " --- indent ---
+    #     set smartindent
+    #     set expandtab
+    #     " --- keymap ---
+    #     " jk as esc
+    #     inoremap <silent> jk <Esc>
+    #   '';
 
     plugins = with pkgs.vimPlugins; [
       onedark-vim
 
+      comment-nvim
+      vim-closer
+      editorconfig-nvim
+      vim-trailing-whitespace
+      telescope-nvim
+      telescope-fzf-native-nvim
+      telescope-ui-select-nvim
+
+
+      nvim-treesitter.withAllGrammars
+      nvim-treesitter-refactor
       vim-nix
       vim-cue
       vim-markdown
+      vim-fish
+      vim-go
 
 
-      auto-pairs
+
+
+
+
+
       (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
 
       # coc-nvim
