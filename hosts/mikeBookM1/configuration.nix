ae0c0479960c4b1bf55d8302ae60e9573bf1d991
Mike Kantzer <mbkantzer@gmail.com>
2023-03-28 12:53:59 -0400

    Components for new M1 mac

---
+++ hosts/mikeBookM1/configuration.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../../modules
+    ../../modules/fonts
+    ../../modules/darwin-settings
+
+    ./users/mk5r.nix
+  ];
+
+  # Set your time zone.
+  # time.timeZone = "America/New_York";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  #environment.systemPackages = with pkgs; [];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  # programs.mosh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = 4; # Did you read the comment?
+}
