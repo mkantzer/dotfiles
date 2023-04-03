d7670e533f74130e60ac169531db7d9a0cb2930c
mk5r <github@kantzer.io>
2023-04-03 12:51:54 -0400

    Remove authorized keys file: permissions issue

--- home/mk5r/global/default.nix
+++ home/mk5r/global/default.nix
@@ -20,10 +20,14 @@ in
 
   home.file.".colorscheme".text = config.colorscheme.slug;
 
-  home.file.".ssh/authorized_keys".text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9K7mYtXECI6LD8iptulJC3eXZL4fE+M1M24UftlcnX mbkantzer@gmail.com
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFnW0M2ZOK4/q5zQgMb03Cvq+DkVGaOEcqbozJXbVWh
-  '';
+  # home.file.".ssh/authorized_keys" = {
+  #   enable = true;
+  #   executable = false;
+  #   text = ''
+  #     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9K7mYtXECI6LD8iptulJC3eXZL4fE+M1M24UftlcnX mbkantzer@gmail.com
+  #     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFnW0M2ZOK4/q5zQgMb03Cvq+DkVGaOEcqbozJXbVWh
+  #   '';
+  # };
 
   programs = {
     home-manager.enable = true;
